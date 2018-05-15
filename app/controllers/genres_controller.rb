a892d8bd8ed404da182aeac82bd18fef006d2b39
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-15 20:13:48 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -22,6 +22,11 @@ class GenresController < ApplicationController
     redirect_to genre_path(@genre)
   end
 
+  def edit 
+    @genre = Genre.find(params[:id])
+
+  end
+
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
