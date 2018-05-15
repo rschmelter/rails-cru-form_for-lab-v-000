90d370f21e4926c249db0dc8f21f6c154c4bc32c
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-15 19:41:22 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,20 @@
-class ArtistsController < ApplicationController 
+class ArtistsController < ApplicationController
 
-  
+  def index 
+
+    @artists = Artist.all
+
+  end 
+
+  def show 
+    @artist = artist.find(params[:id])
+    
+  end
+
+  def new 
+    @artist = Artist.new
+
+  end
 
 
 end
