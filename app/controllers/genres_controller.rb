96708774e31fa5898339150962813d5ee2a20d4c
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-15 20:02:11 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,20 @@
 class GenresController < ApplicationController
 
+  def index 
+    @genre = Genre.new 
 
+  end
+
+  def show 
+
+    @genre = Genre.find(params[:id])
+    
+  end
+
+  def new 
+    @genre = Genre.new 
+
+  end
 
 
 end
