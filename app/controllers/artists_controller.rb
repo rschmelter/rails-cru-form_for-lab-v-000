c260d3e14e0129c466be4d627b7fca1a57cb74c5
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-15 19:53:02 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -23,6 +23,18 @@ class ArtistsController < ApplicationController
 
   end
 
+  def edit 
+    @artist = Artist.find(params[:id])
+
+  end
+
+  def update 
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+
+  end
+
   private
 
     def artist_params
